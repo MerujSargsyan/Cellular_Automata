8b2a8170cdd740dcb24bba78a6b1e3c1fa151df6
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-07-24 10:15:39 -0700

    squares drawn

---
+++ build/CMakeFiles/CellularAutomata.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CellularAutomata.dir/src/main.c.o"
+  "CMakeFiles/CellularAutomata.dir/src/main.c.o.d"
+  "bin/CellularAutomata"
+  "bin/CellularAutomata.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CellularAutomata.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.c
+++ src/main.c
@@ -1,7 +1,38 @@
 #include <raylib.h>
 #include <stdio.h>
 
+const int sq_width = 50;
+const int sq_height = 50;
+const int w_width = 800;
+const int w_height = 800;
+
+void DrawRows() {
+    int curr = 0;
+    for(int i = 0; i < w_height; i += sq_height) {
+        DrawLine(0, i, w_width, i, WHITE);
+    }
+}
+
+void DrawCols() {
+    int curr = 0;
+    for(int i = 0; i < w_height; i += sq_width) {
+        DrawLine(i, 0, i, w_height, WHITE);
+    }
+}
+
 int main(void) {
-    printf("Hello World!\n");
+    SetTargetFPS(30);
+
+    InitWindow(w_width, w_height, "Cellular Automata");
+    while(!WindowShouldClose()) {
+        ClearBackground(BLACK);
+        BeginDrawing();
+         {
+             DrawRows();
+             DrawCols();
+         }
+        EndDrawing();
+    }
+    CloseWindow();
     return 0;
 }
