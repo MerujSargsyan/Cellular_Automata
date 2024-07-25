a920e05f0a2312665dbaf60ad30394d2c790be98
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-07-24 17:28:52 -0700

    drawing squares & random start

--- src/main.c
+++ src/main.c
@@ -1,12 +1,15 @@
 #include <raylib.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 const int sq_width = 10;
 const int sq_height = 10;
 const int w_width = 800;
 const int w_height = 800;
 const int sq_count = (w_width / sq_width) * (w_height / sq_height);
+const int rows = sq_count / (w_width / sq_width);
+const int cols = sq_count / (w_height/ sq_height);
 
 void DrawRows() {
     int curr = 0;
@@ -33,8 +36,6 @@ void DrawCols() {
  * 1 0 0 0 0 1 0 0 0 0 1 0 0 0 0 1
 */
 void DrawCells(int* cells) {
-    int rows = sq_count / (w_width / sq_width);
-    int cols = sq_count / (w_height/ sq_height);
     for(int r = 0; r < rows; r++) {
         for(int c = 0; c < cols; c++) {
             if(cells[r * cols + c]) {
@@ -44,10 +45,23 @@ void DrawCells(int* cells) {
     }
 }
 
+int* InitGrid(int start_count) {
+    int* cells = malloc(sizeof(int) * sq_count);
+    for(int i = 0; i < sq_count; i++) {
+        cells[i] = 0;
+    }
+    srand(time(0));
+    for(int i = 0; i < start_count; i++) {
+        int idx = rand() % sq_count;
+        cells[idx] = 1;
+    }
+    return cells;
+}
+
 int main(void) {
     SetTargetFPS(30);
 
-    int* cells = malloc(sizeof(int) * sq_count);
+    int* cells = InitGrid(500);
 
     InitWindow(w_width, w_height, "Cellular Automata");
     while(!WindowShouldClose()) {
@@ -61,5 +75,6 @@ int main(void) {
         EndDrawing();
     }
     CloseWindow();
+    free(cells);
     return 0;
 }
