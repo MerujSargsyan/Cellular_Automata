85956094d94a7f48c353dcd9c690538d70c82644
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-07-26 10:20:09 -0700

    not taouching release version

--- src/main.c
+++ src/main.c
@@ -1,85 +1,26 @@
-#include <raylib.h> 
+#include <raylib.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <string.h>
-
-#define MAX_LEN 10
 
 const int sq_width = 10;
 const int sq_height = 10;
 const int w_width = 800;
 const int w_height = 800;
-const int start_count = 500;
 const int sq_count = (w_width / sq_width) * (w_height / sq_height);
 const int rows = sq_count / (w_width / sq_width);
 const int cols = sq_count / (w_height/ sq_height);
-int live = 0;
-int paused = 0;
-
-const int button_height = 50;
-const int button_width = 200;
-
-struct button {
-    char* text;
-    Rectangle rect;
-};
-
-struct button play_pause;
-struct button start_stop;
-
-void init_buttons() {
-    play_pause.text = malloc(MAX_LEN);
-    strncpy(play_pause.text, "Pause", strnlen("Pause", MAX_LEN));
-    play_pause.rect = (Rectangle){(w_width/4) - (button_width/2), w_height, button_width, button_height};
-    
-    start_stop.text = malloc(MAX_LEN);
-    strncpy(start_stop.text, "Start", strnlen("Start", MAX_LEN));
-    start_stop.rect = (Rectangle){(w_width - w_width/4) - (button_width/2), w_height, button_width, button_height};
-}
-
-void update_button_text() {
-    if(paused) {
-        strncpy(play_pause.text, "Play\0", strnlen("Play\0", MAX_LEN));
-    } else {
-        strncpy(play_pause.text, "Pause\0", strnlen("Pause\0", MAX_LEN));
-    }
-
-    if(live) {
-        strncpy(start_stop.text, "Stop\0", strnlen("Stop\0", MAX_LEN));
-    } else {
-        strncpy(start_stop.text, "Start\0", strnlen("Start\0", MAX_LEN));
-    }
-}
-
-void update_buttons(Vector2 point) {
-    if(CheckCollisionPointRec(point, play_pause.rect) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-        paused = !paused;
-        update_button_text();
-    }
-
-    if(CheckCollisionPointRec(point, start_stop.rect) && IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-        live = !live;
-        update_button_text();
-    }
-
-    DrawRectangleRec(play_pause.rect, BLUE);
-    DrawText(play_pause.text, play_pause.rect.x + button_width/2 - MeasureText(play_pause.text, 12), play_pause.rect.y + button_height/2, 12, WHITE);
-
-    DrawRectangleRec(start_stop.rect, BLUE);
-    DrawText(start_stop.text, start_stop.rect.x + button_width/2 - MeasureText(start_stop.text, 12), start_stop.rect.y + button_height/2, 12, WHITE);
-}
 
 void draw_rows() {
     int curr = 0;
-    for(int i = 0; i <= w_height; i += sq_height) {
+    for(int i = 0; i <=w_height; i += sq_height) {
         DrawLine(0, i, w_width, i, WHITE);
     }
 }
 
 void draw_cols() {
     int curr = 0;
-    for(int i = 0; i <= w_height; i += sq_width) {
+    for(int i = 0; i < w_height; i += sq_width) {
         DrawLine(i, 0, i, w_height, WHITE);
     }
 }
@@ -106,18 +47,15 @@ void draw_cells(int* cells) {
     }
 }
 
-void start_life(int* cells) {
-    srand(time(0));
-    for(int i = 0; i < start_count; i++) {
-        int idx = rand() % sq_count;
-        cells[idx] = 1;
-    }
-}
-
 int* init_grid(int start_count) {
     int* cells = malloc(sizeof(int) * sq_count);
     for(int i = 0; i < sq_count; i++) {
         cells[i] = 0;
+    }
+    srand(time(0));
+    for(int i = 0; i < start_count; i++) {
+        int idx = rand() % sq_count;
+        cells[idx] = 1;
     }
     return cells;
 }
@@ -155,35 +93,19 @@ void apply_rules(int* cells) {
     }
 }
 
-void read_input() {
-    if(IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-        live = !live;
-    }
-}
-
-void update(int* cells) {
-    draw_rows();
-    draw_cols();
-    update_buttons(GetMousePosition());
-    read_input();
-    if(live) {
-        apply_rules(cells);
-        draw_cells(cells);
-    }
-}
-
 int main(void) {
-    SetTargetFPS(5);
+    SetTargetFPS(10);
 
-    int* cells = init_grid(start_count);
-    InitWindow(w_width, w_height + button_height, "Cellular Automata");
-    init_buttons();
+    int* cells = init_grid(500);
+    InitWindow(w_width, w_height, "Cellular Automata");
     while(!WindowShouldClose()) {
         ClearBackground(BLACK);
-        // read_input(cells);
         BeginDrawing();
          {
-             update(cells);
+             draw_cells((int*)cells);
+             draw_rows();
+             draw_cols();
+             apply_rules(cells);
          }
         EndDrawing();
     }
